a7322135a8519a044111477a0ba8cca87478ab10
Samuel <srowbotham@me.com>
2022-07-10 13:27:32 +0100

    Inital file reading/parameter handling

--- file_transfer_client/src/main.rs
+++ file_transfer_client/src/main.rs
@@ -1,3 +1,37 @@
+
+
+use std::env;
+
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+// Invoke as echo <interface name>
 fn main() {
-    println!("Hello, world!");
+
+    let path_string = match env::args().nth(1) {
+        Some(value) => value,
+        None => "test.txt".to_string(),
+    };
+
+    let ip_string = match env::args().nth(1) {
+        Some(value) => value,
+        None => "127.0.0.1".to_string(),
+    };
+
+
+    let path = Path::new(&path_string);
+    let display = path.display();
+    
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    };
+
 }
